653fdc8cacdc301828309989e2e5debd2e8593be
Gonho LEE <sincerity410@gmail.com>
2020-09-18 22:22:31 +0900

    Add hotter.py

---
+++ Programmers/hotter.py
@@ -0,0 +1,14 @@
+def solution(scoville, K):
+    sc  = -1
+    cnt = 0
+    while True:
+        scoville.sort()
+        if scoville[0] >= K:
+            break
+        try:
+            scoville.append(scoville.pop(0) + scoville.pop(0)*2)
+        except IndexError:
+            return -1
+        cnt += 1
+
+    return cnt
